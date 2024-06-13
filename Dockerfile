997d9d1bf9cde3fc55a727ef789d8a3527ebf4ce
Aliou Bop <bopaliou1994@gmail.com>
2024-06-13 18:40:14 +0000

    Configuration du projet avec Docker

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+# Utilisez l'image officielle de Python comme base
+FROM python:3
+
+# Variables d'environnement pour Python
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
+
+
+# Créez et définissez le répertoire de travail dans le conteneur Docker
+WORKDIR /app
+
+# Installez les dépendances Python
+COPY requirements.txt /app/
+RUN pip install --upgrade pip && pip install -r requirements.txt
+
+# Copiez tous les fichiers de votre projet dans le répertoire de travail du conteneur Docker
+COPY . /app/
--- SystemeGestionVisiteurs/settings.py
+++ SystemeGestionVisiteurs/settings.py
@@ -11,7 +11,13 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+import environ
 
+env=environ.Env(
+    DEBUG=(bool,False)
+)
+environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-dr+h8o!1bv=g7!nx(44)mdl9%mhgjipmd!x4=pr)8cled%%k-)"
+SECRET_KEY = env('SECRET_KEY')
+DEBUG = env('DEBUG')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 
 # Application definition
@@ -37,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -74,10 +79,7 @@ WSGI_APPLICATION = "SystemeGestionVisiteurs.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': env.db()
 }
 
 
